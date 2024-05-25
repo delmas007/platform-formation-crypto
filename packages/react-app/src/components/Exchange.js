2f6f400f1f06ed83a84c3cd6591a266459ab9c89
delmas <azertyazerty1ze@gmail.com>
2024-05-25 23:34:12 +0000

    affichage

---
+++ packages/react-app/src/components/Exchange.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Exchange(props) {
+    return (
+        <div>Exchange</div>
+    );
+}
+
+export default Exchange;
---
+++ packages/react-app/src/components/WalletButton.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function WalletButton(props) {
+    return (
+        <div>WalletButton</div>
+    );
+}
+
+export default WalletButton;

226b33f3ffa6059a665e43324e0a1baa9dd24639
delmas007 <azertyazerty1ze@gmail.com>
2024-05-27 09:49:33 +0000

    WalletButton

--- packages/react-app/src/components/WalletButton.js
+++ packages/react-app/src/components/WalletButton.js
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, {useEffect,useState} from 'react';
+import {shortenAddress,useEthers,useLookupAddress} from "@usedapp/core";
+import styles from '../styles';
 
 function WalletButton(props) {
+    const [accountAdress, setAccountAdress] = useState("");
+    const {ens} = useLookupAddress();
+    const {account, activateBrowserWallet, deactivate} = useEthers();
+
+    useEffect(() => {
+        if(ens){
+            setAccountAdress(ens);
+        }else if(account){
+            setAccountAdress(shortenAddress(account));
+        }else{
+            setAccountAdress("");
+        }
+
+    }, [account,ens, setAccountAdress]);
     return (
-        <div>WalletButton</div>
+        <button
+            onClick={() => {
+                if (!account) {
+                    activateBrowserWallet();
+                } else {
+                    deactivate();
+                }
+            }}
+            className={styles.walletButton}
+        >
+            {accountAdress || "Connect Wallet"}
+        </button>
     );
 }
 

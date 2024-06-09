9c56ba3bda0d2f673d1a0c1f9c16fe3065d47921
delmas <azertyazerty1ze@gmail.com>
2024-06-09 20:26:15 +0000

    tuto

--- packages/react-app/src/Tutoriat/tutoriat.js
+++ packages/react-app/src/Tutoriat/tutoriat.js
@@ -1,50 +1,34 @@
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
 import { Header } from "../header";
 import s from "./style.module.css";
 import {useForm} from "react-hook-form"; // Import the CSS module
 import { jwtDecode } from 'jwt-decode';
 
 
-const courses = [
-  {
-    university: "UPB",
-    courses: [
-      {
-        no: "CS101",
-        title: "Introduction to Computer Science",
-        description: "Basic concepts of computer science.",
-        cost: "10000 FCFA"
-      },
-      {
-        no: "CS102",
-        title: "Data Structures and Algorithms",
-        description: "Introduction to data structures and algorithms.",
-        cost: "15000 FCFA"
-      }
-    ]
-  },
-  {
-    university: "Another Polytechnic University",
-    courses: [
-      {
-        no: "IT201",
-        title: "Database Management Systems",
-        description: "Overview of database management systems.",
-        cost: "12000 FCFA"
-      },
-      {
-        no: "IT202",
-        title: "Web Development",
-        description: "Introduction to web development.",
-        cost: "13000 FCFA"
-      }
-    ]
-  }
-];
+
 
 export function Tutoriat() {
   const { register, handleSubmit, setValue, formState: { errors } } = useForm();
   const [showModal, setShowModal] = useState(false);
+  const [showModale, setShowModale] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [publications, setPublications] = useState([]);
+  const [showAlert, setShowAlert] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    setIsLoggedIn(!!token);
+    fetch('http://localhost:5050/tutos/')
+        .then(response => response.json())
+        .then(data => {
+          // Trier les publications par date de publication (du plus récent au plus ancien)
+          const sortedData = data.sort((a, b) => new Date(b.datePublication) - new Date(a.datePublication));
+          setPublications(sortedData);
+
+        })
+        .catch(error => console.error('Error fetching data:', error));
+  }, [showModale===true]);
+
 
   const onSubmit = (data) => {
     const token = localStorage.getItem('token');
@@ -69,7 +53,9 @@ export function Tutoriat() {
       })
           .then(response => {
             if (response.ok) {
+              setShowModale(true)
               console.log('Tutorial added successfully');
+              setShowModale(false )
             } else {
               console.error('Failed to add tutorial');
             }
@@ -81,31 +67,73 @@ export function Tutoriat() {
   };
 
   const toggleModal = () => {
-    setShowModal(!showModal);
+    if (isLoggedIn) {
+      setShowModal(!showModal);
+    } else {
+      setShowAlert(true);
+    }
   };
 
   return (
       <div className="font-sans p-6 bg-gray-100 min-h-screen">
         <Header />
         <div className="max-w-7xl mx-auto">
-          <button onClick={toggleModal} className="mt-4 py-2 px-4 bg-blue-600 text-white rounded-md">
-            Inscrire à un cours
+          <button
+              onClick={toggleModal}
+              className={`mt-4 py-2 px-4 rounded-md bg-blue-600 text-white`}
+
+          >
+            Ajouter un cours
           </button>
-          <h1 className="text-3xl font-bold text-center mb-8">Cours Offerts</h1>
-          {courses.map((university) => (
-              <div key={university.university} className="mb-8">
-                <h2 className="text-2xl font-semibold mb-4">{university.university}</h2>
-                <ul>
-                  {university.courses.map((course) => (
-                      <li key={course.no} className="mb-4 p-4 bg-white rounded-lg shadow-md">
-                        <h3 className="text-xl font-semibold">{course.title}</h3>
-                        <p>{course.description}</p>
-                        <p className="font-bold">Coût mensuel: {course.cost}</p>
-                      </li>
-                  ))}
-                </ul>
+          {showAlert && (
+              <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded-md my-4">
+                <p>Veuillez vous connecter ou vous inscrire pour pouvoir publier.</p>
               </div>
-          ))}
+          )}
+          <h1 className="text-3xl font-bold text-center mb-8">Cours Offerts</h1>
+          <div className="p-4 min-h-screen bg-gray-100">
+            <div className="max-w-7xl mx-auto">
+              <h1 className="text-3xl font-bold text-center mb-8 text-blue-600">Liste des Publications</h1>
+              {publications.map((publication, index) => (
+                  <div key={index} className="mb-8 p-6 bg-white rounded-lg shadow-lg border border-gray-200">
+                    <div className="flex items-center mb-4">
+                      <div
+                          className="w-12 h-12 rounded-full bg-gray-300 flex items-center justify-center overflow-hidden">
+                        {publication.etudiant.photo ?
+                            <img src={publication.etudiant.photo} alt="Profile"
+                                 className="w-full h-full object-cover"/> :
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                 stroke="currentColor" className="w-6 h-6 text-gray-600">
+                              <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M12 11.25a3.75 3.75 0 100-7.5 3.75 3.75 0 000 7.5zm.75 6h-1.5A6.75 6.75 0 004.5 18h15a6.75 6.75 0 00-6.75-6.75z"/>
+                            </svg>
+                        }
+                      </div>
+                      <div className="ml-4">
+                        <h2 className="text-xl font-semibold">{publication.etudiant.prenom} {publication.etudiant.nom}</h2>
+                        <p className="text-gray-500">{new Date(publication.datePublication).toLocaleString()}</p>
+                      </div>
+                    </div>
+                    <h3 className="text-2xl font-semibold mb-2 text-blue-700">{publication.titre}</h3>
+                    <p className="mb-4 text-gray-700">{publication.description}</p>
+                    {publication.etudiant.telephone && (
+                        <p className="mb-2 text-gray-900"><strong>Téléphone:</strong> {publication.etudiant.telephone}
+                        </p>
+                    )}
+                    {publication.coutMensuel && (
+                        <p className="mb-2 text-gray-900"><strong>Coût mensuel::</strong> {publication.coutMensuel} fcfa
+                        </p>
+                    )}
+                    <p className="mb-2 text-gray-900"><strong>Email:</strong> {publication.etudiant.email}</p>
+                    <p className="mb-2 text-gray-900"><strong>Université:</strong> {publication.etudiant.universite}</p>
+                    {publication.etudiant.societe && (
+                        <p className="mb-2 text-gray-900"><strong>Société:</strong> {publication.etudiant.societe}</p>
+                    )}
+                  </div>
+              ))}
+            </div>
+          </div>
+
           {showModal && (
               <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
                 <div className="bg-white p-6 rounded-lg shadow-md">
--- packages/react-app/src/header.jsx
+++ packages/react-app/src/header.jsx
@@ -1,39 +1,61 @@
 import s from "./style.module.css"
-import { Link, useLocation  } from "react-router-dom"
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import {useEffect, useState} from "react";
 
 
 export function Header(){
     const location = useLocation();
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        // Vérifier si l'utilisateur est connecté
+        const token = localStorage.getItem('token');
+        setIsLoggedIn(!!token);
+    }, []);
+
+    const handleLogout = () => {
+        localStorage.removeItem('token');
+        setIsLoggedIn(false);
+        navigate('/connexion');
+    };
     return (
         <header className={s.AppHeader}>
-        <div className={s.HeaderButtons}>
-        {location.pathname !== "/connexion" && ( // Vérifie si l'URL est différente de "/connexion"
-        <Link to="/connexion"><button className={s.Button}>Connexion</button>
-        </Link>
-         )}
-        {location.pathname !== "/inscription" && ( // Vérifie si l'URL est différente de "/inscription"
-          <Link to="/inscription">
-            <button className={s.Button}>Inscription</button>
-          </Link>
-        )}
-        </div>
-        <h1>Bienvenue sur la plateforme YTG</h1>
-        <p>
-        {location.pathname !== "/" && (
-           <Link to="/">Accueil</Link>  
-        )}
-        {location.pathname !== "/" && " | "}
-        {location.pathname !== "/formations" && (
-        <Link to="/formations">Formations</Link> 
-        )}
-        {location.pathname !== "/formations" && " | "} 
-        {location.pathname !== "/tutoriat" && (
-        <Link to="/tutoriat">Tutorat</Link>  
-        )}
-        {location.pathname !== "/tutoriat" && " | "}
-        <Link to="/">Consultation</Link>  |
-            {location.pathname !== "/cryptomonnaie" && (
-        <Link to="/cryptomonnaie">Gestion de Cryptomonnaie</Link>
+            <div className={s.HeaderButtons}>
+                <div className="flex justify-end space-x-4 mb-4">
+                    {!isLoggedIn && location.pathname !== "/connexion" && (
+                        <Link to="/connexion">
+                            <button className={s.Button}>Connexion</button>
+                        </Link>
+                    )}
+                    {!isLoggedIn && location.pathname !== "/inscription" && (
+                        <Link to="/inscription">
+                            <button className={s.Button}>Inscription</button>
+                        </Link>
+                    )}
+                    {isLoggedIn && (
+                        <button onClick={handleLogout} className={s.Button}>Déconnexion</button>
+
+                    )}
+                </div>
+            </div>
+            <h1>Bienvenue sur la plateforme YTG</h1>
+            <p>
+                {location.pathname !== "/" && (
+                    <Link to="/">Accueil</Link>
+                )}
+                {location.pathname !== "/" && " | "}
+                {location.pathname !== "/formations" && (
+                    <Link to="/formations">Formations</Link>
+                )}
+                {location.pathname !== "/formations" && " | "}
+                {location.pathname !== "/tutoriat" && (
+                    <Link to="/tutoriat">Tutorat</Link>
+                )}
+                {location.pathname !== "/tutoriat" && " | "}
+                <Link to="/">Consultation</Link> |
+                {location.pathname !== "/cryptomonnaie" && (
+                    <Link to="/cryptomonnaie">Gestion de Cryptomonnaie</Link>
             )}
             {location.pathname !== "/cryptomonnaie" && " | "}
         <Link to="/about">A Propos</Link> 
